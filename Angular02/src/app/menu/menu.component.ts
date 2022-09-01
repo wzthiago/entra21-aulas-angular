d1f74827cdad95ab4aa51fd32029ac8725db3760
wzthiago <wzthiago@gmail.com>
2022-09-01 19:15:57 -0300

    Atualizando...

--- Angular02/src/app/menu/menu.component.ts
+++ Angular02/src/app/menu/menu.component.ts
@@ -7,9 +7,63 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
+  links!: Array<any>;
+
   constructor() { }
 
   ngOnInit(): void {
-  }
+    this.links = new Array();
+    this.links.push({
+      rota: "vaiPagina",
+      textContent: "Página",
+      cor: "nav-icon far fa-circle text-success"
+    });
 
+    this.links.push({
+      rota: "outroComponente",
+      textContent: "Outro",
+      cor: "nav-icon far fa-circle text-danger"
+    });
+    this.links.push({
+      rota: "inicioComponente",
+      textContent: "Inicio Componente",
+      cor: "nav-icon far fa-circle text-danger"
+    });
+    this.links.push({
+      rota: "property",
+      textContent: "Property",
+      cor: "nav-icon far fa-circle text-warning"
+    });
+    this.links.push({
+      rota: "event",
+      textContent: "Event",
+      cor: "nav-icon far fa-circle text-light"
+    });
+    this.links.push({
+      rota: "mao-dupla",
+      textContent: "Mão Dupla",
+      cor: "nav-icon far fa-circle text-warning"
+    });
+    this.links.push({
+      rota: ["receber", "Thiago", 31, "Fpolis"],
+      textContent: "Receber",
+      cor: "nav-icon far fa-circle text-danger"
+    });
+    this.links.push({
+      rota: "enviar",
+      textContent: "Enviar Componet",
+      cor: "nav-icon far fa-circle text-success"
+    });
+    this.links.push({
+      rota: "diretivaIf",
+      textContent: "Diretiva If",
+      cor: "nav-icon far fa-circle text-danger"
+    });
+    this.links.push({
+      rota: "diretivaFor",
+      textContent: "Diretiva For",
+      cor: "nav-icon far fa-circle text-danger"
+    });
+  }
 }
+
